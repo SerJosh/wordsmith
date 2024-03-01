0c783b16cc6c072816c10dae4a2ef88cb81c8a8b
SerJosh <joshronan25@gmail.com>
2024-03-01 22:51:35 +0000

    add and secure secret key

--- wordsmith/settings.py
+++ wordsmith/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qa_n80zi-&#l0r0fgut+8-oy7s+fq20%hk$&*%snyy_fzri#ew'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
