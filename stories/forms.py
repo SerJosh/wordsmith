76cfe1b862565ae9722941d496edc6a0353f1f25
SerJosh <joshronan25@gmail.com>
2024-03-17 23:50:04 +0000

    Edit styling of index page and start styling create story page

--- stories/forms.py
+++ stories/forms.py
@@ -3,23 +3,15 @@ from django.forms import ModelForm
 from .models import Story
 from django_summernote.widgets import SummernoteWidget
 
-# class NameForm(forms.Form):
-#     your_name = forms.CharField(label="Your name", max_length=100)
 
-# class StoryForm(forms.Form):
-#     Title = forms.CharField(max_length=20)
-#     Blurb = forms.CharField(max_length=200, widget=forms.Textarea )
-#     Story = forms.CharField(widget=SummernoteWidget)
-
-# Create Story Form
 class StoryForm(ModelForm):
     class Meta:
         model = Story
         fields = ('Title', 'Blurb', 'Content', 'Story_Cover', )
 
         widgets = {
-            'Title': forms.TextInput(attrs={'class': 'form-control'}),
-            'Blurb': forms.TextInput(attrs={'class': 'form-control'}),
+            'Title': forms.TextInput(attrs={'class': 'form-control w-25  text-center'}),
+            'Blurb': forms.Textarea(attrs={'class': 'form-control w-50 text-center', "rows":4, "cols":10}),
             'Content': forms.TextInput(attrs={'class': 'form-control'})
         }
 
--- stories/models.py
+++ stories/models.py
@@ -1,21 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
-# from django_summernote.widgets import SummernoteWidget
 
-
-# Create your models here.
-# class WordsmithUser (models.Model):
-#     name = models.CharField(max_length=20)
-#     email = models.EmailField('User Email', max_length=40)
-
-#     def __str__(self):
-#         return self.name
 
 class Story(models.Model):
     Title = models.CharField('Title', max_length=20)
     Story_Cover = models.ImageField(null=True, blank=True, upload_to="images/")
     Auther = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    Blurb = models.CharField('Blurb', max_length=200, blank=True)
+    Blurb = models.TextField('Blurb', max_length=200, blank=True)
     Content = models.TextField('Short Story', max_length=50000)
 
 
