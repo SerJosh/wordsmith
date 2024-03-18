c5ece7da53446487f3847809d7ac2851acb4d920
SerJosh <joshronan25@gmail.com>
2024-03-18 23:00:24 +0000

    add placeholder image to story cover if not given a cover

--- stories/models.py
+++ stories/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class Story(models.Model):
     Title = models.CharField('Title', max_length=20)
-    Story_Cover = models.ImageField(null=True, blank=True, upload_to="images/")
+    Story_Cover = CloudinaryField('image', default='placeholder')
     Auther = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     Blurb = models.TextField('Blurb', max_length=200, blank=True)
     Content = models.TextField('Short Story', max_length=50000)
