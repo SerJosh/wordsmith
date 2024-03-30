b8cc2fd10bf525786c0dcc803fcf66c3ccfce540
SerJosh <joshronan25@gmail.com>
2024-03-30 00:24:15 +0000

    Edit javascript code and put debug to false for heroku deployment

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,24 @@
 //Click Event for Delete Button
-document.getElementById("deletor").addEventListener("click", function () {
+// document.getElementById("deletor").addEventListener("click", function () {
+//     document.getElementById("hide").style.display = 'block';
+//     document.getElementById("show-delete").style.display = 'none';
+
+// });
+function displayDelete() {
     document.getElementById("hide").style.display = 'block';
     document.getElementById("show-delete").style.display = 'none';
 
-});
+};
 
-//Click Event for Cancel Button
-document.getElementById("cancelor").addEventListener("click", function () {
+function cancelDelete() {
     document.getElementById("hide").style.display = 'none';
     document.getElementById("show-delete").style.display = 'block';
 
-});
+};
+
+//Click Event for Cancel Button
+// document.getElementById("cancelor").addEventListener("click", function () {
+//     document.getElementById("hide").style.display = 'none';
+//     document.getElementById("show-delete").style.display = 'block';
+
+// });
